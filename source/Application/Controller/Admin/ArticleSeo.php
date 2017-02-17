2492bd79c743d978d2d2775a85c2a52e9987e954
sebbbbauer <sebastian.bauer@oxid-esales.com>
2017-02-17 16:07:46 +0100

    ESDEV-4303 remove unused use of ObjectSeo

--- source/Application/Controller/Admin/ArticleSeo.php
+++ source/Application/Controller/Admin/ArticleSeo.php
@@ -25,7 +25,6 @@ namespace OxidEsales\EshopCommunity\Application\Controller\Admin;
 use oxRegistry;
 use oxDb;
 use oxField;
-use OxidEsales\Eshop\Application\Controller\Admin\ObjectSeo;
 
 /**
  * Article seo config class
